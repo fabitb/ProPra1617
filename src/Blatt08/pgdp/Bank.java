2416cc5c86e1313116907156817669969dc80316
Fabian Thomas-Barein <fabian.thomas-barein@tum.de>
2016-12-13 19:56:08 +0100

    added Exercise 8.6

---
+++ src/Blatt08/pgdp/Bank.java
@@ -0,0 +1,31 @@
+package Blatt08.pgdp;
+
+public class Bank {
+
+    private class BankAccountList{
+        public BankAccount info;
+        public BankAccountList next;
+    }
+
+    private BankAccountList accounts;
+
+    public int newAccount (String firstname, String lastname){
+
+    }
+
+    public void removeAccount(int accountnumber){
+
+    }
+
+    public Money getBalance(int accountnumber){
+
+    }
+
+    public boolean depositOrWithdraw(int accountnumber, Money m){
+
+    }
+
+    public boolean transfer (int from, int to, Money m){
+
+    }
+}
